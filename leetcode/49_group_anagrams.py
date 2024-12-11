1879a2795bad92f9f8e72546302cbd2e26dd8f3b
Sydney McClements <smccl96@gmail.com>
2024-12-10 22:35:31 -0800

    49. Group Anagrams

--- leetcode/49_group_anagrams.py
+++ leetcode/49_group_anagrams.py
@@ -1,28 +1,28 @@
-"""49. Group Anagrams"""
-
 from collections import defaultdict
 from typing import List
 
 class Solution:
-    """Solution Class"""
 
-    def group_anagrams(self, strs: List[str]) -> List[List[str]]:
-        """
-        This function groups anagrams by counting the respective characters by their ASCII ordering
-        in an array. Then, serializing them into tuples so that it's hashable. Thus, any string
-        that serializes to an existing encoding will be added to that collection. Finally, we
-        return the groupings to the caller.
-        """
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """Groups anagram strings by calculating the character frequency of each string and using the frequency as a dictionary key to map anagrams together.
 
-        anagrams_map = defaultdict(list)
+        Args:
+            strs (List[str]): A list of input strings.
+
+        Returns:
+            List[List[str]]: A list of groups, where each group is a list of strings that are anagrams of each other.
+        """
+        anagrams = defaultdict(list)
 
         for word in strs:
-            char_count = [0] * 26
+
+            char_count = [0]*26
+
             for char in word:
-                index = ord(char) - ord('a')
-                char_count[index] += 1
-            key = tuple(char_count)
+                char_count[ord(char) - ord('a')] += 1
 
-            anagrams_map[key].append(word)
+            char_count = ''.join(chr(i + ord('a')) * char_count[i] for i in range(26))
 
-        return list(anagrams_map.values())
+            anagrams[char_count].append(word)
+
+        return list(anagrams.values())
