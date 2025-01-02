4dfe4c257542e2409cb3ab08c01018e787416585
Sydney <smccl96@gmail.com>
2025-01-01 16:00:03 -0800

    directions array and helper function for boundaries

--- leetcode/54_spiral_matrix.py
+++ leetcode/54_spiral_matrix.py
@@ -13,44 +13,48 @@ class Solution:
 
         m, n = len(matrix), len(matrix[0])
         r, c = 0, 0
-        UP, DOWN, RIGHT, LEFT = 0, 1, 2, 3
-        direction = RIGHT
+        UP = (-1,0)
+        DOWN = (1,0)
+        LEFT = (0,-1)
+        RIGHT = (0,1)
+        DIRECTIONS = [RIGHT, DOWN, LEFT, UP]
+        cur_dir = 0
+
         ans = []
 
         UP_WALL = 0
-        DOWN_WALL = m
-        RIGHT_WALL = n
-        LEFT_WALL = -1
+        DOWN_WALL = m - 1
+        RIGHT_WALL = n - 1
+        LEFT_WALL = 0
+
+        def in_bounds(next_r, next_c):
+            """
+            checks if the next cell will be in bounds.
+            """
+            return UP_WALL <= next_r <= DOWN_WALL and LEFT_WALL <= next_c <= RIGHT_WALL
 
         while len(ans) != m*n:
-            if direction == RIGHT:
-                while c < RIGHT_WALL:
-                    ans.append(matrix[r][c])
-                    c += 1
-                r, c = r + 1, c - 1
-                RIGHT_WALL -= 1
-                direction = DOWN
-            elif direction == DOWN:
-                while r < DOWN_WALL:
-                    ans.append(matrix[r][c])
-                    r += 1
-                r, c = r - 1, c - 1
-                DOWN_WALL -= 1
-                direction = LEFT
-            elif direction == LEFT:
-                while c > LEFT_WALL:
-                    ans.append(matrix[r][c])
-                    c -= 1
-                r, c = r - 1, c + 1
-                LEFT_WALL += 1
-                direction = UP
-            else:
-                while r > UP_WALL:
-                    ans.append(matrix[r][c])
-                    r -= 1
-                r, c = r + 1, c + 1
-                UP_WALL += 1
-                direction = RIGHT
+            ans.append(matrix[r][c])
+
+            dr, dc = DIRECTIONS[cur_dir]
+            next_r, next_c = r + dr, c + dc
+
+            if not in_bounds(next_r, next_c):
+                if cur_dir == 0: #right
+                    UP_WALL += 1
+                elif cur_dir == 1: #down
+                    RIGHT_WALL -= 1
+                elif cur_dir == 2: #left
+                    DOWN_WALL -= 1
+                elif cur_dir == 3: #up
+                    LEFT_WALL += 1
+
+                cur_dir = (cur_dir + 1) % 4
+                dr, dc = DIRECTIONS[cur_dir]
+                next_r, next_c = r + dr, c + dc
+
+            r, c = next_r, next_c
 
         return ans
+
 
