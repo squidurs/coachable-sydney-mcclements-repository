1fee4edcf8339aad5f67996bcdfe7577e5c0ca80
Sydney <smccl96@gmail.com>
2025-01-02 23:30:35 -0800

    visited set for spiral matrix

--- leetcode/54_spiral_matrix.py
+++ leetcode/54_spiral_matrix.py
@@ -19,36 +19,24 @@ class Solution:
         RIGHT = (0,1)
         DIRECTIONS = [RIGHT, DOWN, LEFT, UP]
         cur_dir = 0
-
+        visited = set()
         ans = []
 
-        UP_WALL = 0
-        DOWN_WALL = m - 1
-        RIGHT_WALL = n - 1
-        LEFT_WALL = 0
+        def in_bounds_unvisited(row, col):
+            """
+            checks if the cell is in bounds and is not visited.
+            """
+            return 0 <= row < m and 0 <= col < n and (row, col) not in visited
 
-        def in_bounds(next_r, next_c):
-            """
-            checks if the next cell will be in bounds.
-            """
-            return UP_WALL <= next_r <= DOWN_WALL and LEFT_WALL <= next_c <= RIGHT_WALL
 
         while len(ans) != m*n:
             ans.append(matrix[r][c])
+            visited.add((r, c))
 
             dr, dc = DIRECTIONS[cur_dir]
             next_r, next_c = r + dr, c + dc
 
-            if not in_bounds(next_r, next_c):
-                if cur_dir == 0: #right
-                    UP_WALL += 1
-                elif cur_dir == 1: #down
-                    RIGHT_WALL -= 1
-                elif cur_dir == 2: #left
-                    DOWN_WALL -= 1
-                elif cur_dir == 3: #up
-                    LEFT_WALL += 1
-
+            if not in_bounds_unvisited(next_r, next_c):
                 cur_dir = (cur_dir + 1) % 4
                 dr, dc = DIRECTIONS[cur_dir]
                 next_r, next_c = r + dr, c + dc
