69fcfb0454365eb05e8ec52b8ca7b8e8982a3a33
Sydney McClements <smccl96@gmail.com>
2025-01-23 21:01:07 -0800

    variable name change

--- leetcode/1249_minimum_remove_to_make_valid_parentheses.py
+++ leetcode/1249_minimum_remove_to_make_valid_parentheses.py
@@ -9,25 +9,25 @@ class Solution:
             str: The string with the minimum number of parentheses removed to make it valid.
         """
 
-        stack = []
+        str_builder = []
         left_parentheses = 0
 
         for char in s:
             if char == "(":
                 left_parentheses += 1
-                stack.append(char)
+                str_builder.append(char)
             elif char == ")":
                 if left_parentheses > 0:
                     left_parentheses -= 1
-                    stack.append(char)
+                    str_builder.append(char)
             else:
-                stack.append(char)
+                str_builder.append(char)
 
         if left_parentheses ==  0:
-            return ''.join(stack)
+            return ''.join(str_builder)
 
         res = []
-        for char in reversed(stack):
+        for char in reversed(str_builder):
             if char == "(" and left_parentheses > 0:
                 left_parentheses -= 1
             else:
