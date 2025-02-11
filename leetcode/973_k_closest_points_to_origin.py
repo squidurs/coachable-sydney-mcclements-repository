b540bde0a851b66e5d8b75f0a89bbcdf73784ef1
Sydney McClements <smccl96@gmail.com>
2025-02-10 23:00:49 -0800

    quickselect K Closest Points to Origin

--- leetcode/973_k_closest_points_to_origin.py
+++ leetcode/973_k_closest_points_to_origin.py
@@ -1,5 +1,6 @@
 import math
 import heapq
+from random import randint
 from typing import List
 
 class Solution:
@@ -23,3 +24,63 @@ class Solution:
                 heapq.heappop(max_heap)
 
         return [point for _, point in max_heap]
+
+
+    def kClosest_qs(self, points: List[List[int]], k: int) -> List[List[int]]:
+        """
+        Finds the k closest points to the origin (0,0) using quickselect
+
+        Args:
+            points (List[List[int]]): A list of points represented as [x, y].
+            k (int): The number of closest points to return.
+
+        Returns:
+            List[List[int]]: The k closest points to the origin.
+        """
+
+        def distance(point: List[int]) -> int:
+            """
+            Calculates the distance from the origin.
+
+            Args:
+                point (List[int]): A point represented as [x, y].
+
+            Returns:
+                int: The distance from 'point' to the origin.
+            """
+            return point[0]**2 + point[1]**2
+
+        def quickselect(left: int, right: int, k: int):
+            """
+            Partitions the points so that the k closest points are in the first k positions.
+
+            Args:
+                left (int): Left boundary of the current partition.
+                right (int): Right boundary of the current partition.
+                k (int): The number of closest points to identify.
+            """
+            if left >= right:
+                return
+
+            pivot_idx = randint(left, right)
+            pivot_distance = distance(points[pivot_idx])
+
+            points[pivot_idx], points[right] = points[right], points[pivot_idx]
+
+            i = left
+            for j in range(left, right):
+                if distance(points[j]) <= pivot_distance:
+                    points[i], points[j] = points[j], points[i]
+                    i += 1
+
+            points[i], points[right] = points[right], points[i]
+
+            if i == k:
+                return
+            elif i < k:
+                quickselect(i + 1, right, k)
+            else:
+                quickselect(left, i - 1, k)
+
+        quickselect(0, len(points) - 1, k)
+        return points[:k]
